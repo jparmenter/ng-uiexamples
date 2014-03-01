1f8e797c2b9e13af8b664b743e74e3dc0de51f46
Jeremy Parmenter <shabado9889@gmail.com>
2014-03-01 14:35:15 -0700

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngUiexamplesApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
