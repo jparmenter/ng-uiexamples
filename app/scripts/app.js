2e2ce65ea415522f26558ebecb21b21bcc0b715d
Jeremy Parmenter <shabado9889@gmail.com>
2014-03-02 18:00:03 -0700

    added a new controller

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,19 +1,9 @@
 'use strict';
 
-angular.module('ngUiexamplesApp', [
-  'ngCookies',
-  'ngResource',
-  'ngSanitize',
-  'ngRoute'
-])
+angular.module('ngUiexamplesApp', ['ngCookies', 'ngResource', 'ngSanitize', 'ngRoute'])
   .config(function ($routeProvider, $locationProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-      $locationProvider.html5Mode(true);
+    $routeProvider.when('/', { templateUrl: 'views/main.html', controller: 'MainCtrl'});
+    $routeProvider.when('/about', { templateUrl: 'views/about.html', controller: 'AboutCtrl'});
+    $routeProvider.otherwise({ redirectTo: '/'});
+    $locationProvider.html5Mode(true);
   });
---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ngUiexamplesApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
